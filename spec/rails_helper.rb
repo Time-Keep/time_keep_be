35b1e47d4806d3539510e21be17b08928556f3fa
Eli Fuchsman <efuchsman@gmail.com>
2023-04-25 15:03:40 -0600

    fix vcr filter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,10 +72,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('app_key') { ENV['edamam_api_key'] }
-  config.filter_sensitive_data('app_id') { ENV['edamam_app_id'] }
-  config.filter_sensitive_data('key') { ENV['YOUTUBE_API_KEY'] }
-  config.filter_sensitive_data('client_id') { ENV['unsplash_access_key'] }
+  config.filter_sensitive_data('app_key') { ENV['tom_tom_api_key'] }
   config.default_cassette_options = { record: :new_episodes }
   config.configure_rspec_metadata!
   config.allow_http_connections_when_no_cassette = true

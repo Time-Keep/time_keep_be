5dc0ba51f0bbaf02957af8eaca3ea6fe262c82b8
Eli Fuchsman <efuchsman@gmail.com>
2023-04-25 15:27:14 -0600

    update service call

--- app/services/tom_tom_service.rb
+++ app/services/tom_tom_service.rb
@@ -1,12 +1,10 @@
 class TomTomService
   def self.conn
-    Faraday.new('https://api.tomtom.com/search/2/')
+    Faraday.new('https://api.tomtom.com')
   end
 
-  def self.jewelry_estabs(county_full)
-    county_split_arr = county_full.split
-    rejoin_county = county_split_arr.join("+")
-    response = conn.get("poiSearch/#{rejoin_county}.json?key=#{ENV['tom_tom_api_key']}&countrySet=US&categorySet=9361036")
+  def self.jewelry_estabs(lat,lon)
+    response = conn.get("/search/2/nearbySearch/.json?&key=#{ENV['tom_tom_api_key']}&country=US&categorySet=9361036&lat=#{lat}&lon=#{lon}")
     json = JSON.parse(response.body, symbolize_names: true)
     json[:summary][:totalResults]
   end
--- spec/services/tom_tom_service_spec.rb
+++ spec/services/tom_tom_service_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe "TOMTOM Service" do
   describe "jewelry_estabs" do
     it "returns json of the jewelry establishments for a county full name", :vcr do
-      county_full = "Autauga County"
+      lat = "33.2896"
+      lon = "-87.5251"
 
-      expect(TomTomService.jewelry_estabs(county_full)).to eq(7)
+      expect(TomTomService.jewelry_estabs(lat,lon)).to eq(12)
     end
   end
 end

deaaec731383345a8a76784d868a3851ff9320b0
Anna Marie Sterling <sterling.am@gmail.com>
2023-04-25 22:32:16 -0600

    Add estab count class method

--- app/models/county.rb
+++ app/models/county.rb
@@ -1,15 +1,20 @@
 class County < ApplicationRecord
-    validates :county,
-        :county_ascii,
-        :county_full,
-        :county_fips,
-        :state_id,
-        :state_name,
-        :lat,
-        :lon,
-        :population,
-        :county_unemployment_rate,
-        :state_unemployment,
-        :county_median_income,
-        :tax_rate, presence: true
+  validates :county,
+            :county_ascii,
+            :county_full,
+            :county_fips,
+            :state_id,
+            :state_name,
+            :lat,
+            :lon,
+            :population,
+            :county_unemployment_rate,
+            :state_unemployment,
+            :county_median_income,
+            :tax_rate,
+            presence: true
+
+  def industry_estab_count
+    EstabCountFetcher.fetch_count(self.lat, self.lon)
+  end
 end

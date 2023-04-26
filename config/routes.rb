043b2f2b322ab9503e1c95856f79dcd6e9104db6
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2023-04-26 13:34:24 -0600

    add route for endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+      namespace :v1 do
+        get '/county', to: 'county#index'
+      end
+    end
 end

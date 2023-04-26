32718a7f8f33d7f56b07f677f47da3e1bfc0eb66
Eli Fuchsman <efuchsman@gmail.com>
2023-04-26 13:52:36 -0600

    Create schema dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cmd = 'pg_restore --no-acl --no-owner -h localhost -U $(whoami) -d time_keep_be_development db/data/time_keep_be_development.pgdump'
+puts 'Loading PostgreSQL Data dump into local database with command:'
+puts cmd
+system(cmd)

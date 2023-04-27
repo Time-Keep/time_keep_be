0737eb5047086e5c4f8346b05bcd2ea389e9295e
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2023-04-27 11:59:57 -0600

    error handling

--- app/controllers/api/v1/county_controller.rb
+++ app/controllers/api/v1/county_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::CountyController < ApplicationController
   def index
     county = County.all
-    render json: CountySerializer.counties(county), status: 201
+    if county.empty? 
+        render json: { error: 'API Rate Limit' }, status: 503
+    else
+        render json: CountySerializer.counties(county), status: 200
+    end
   end
 end

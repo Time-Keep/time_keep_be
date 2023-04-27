4651024fe27504e1369aad623c5d3a40ce5d66b5
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2023-04-27 12:16:58 -0600

    add more testing

--- app/controllers/api/v1/county_controller.rb
+++ app/controllers/api/v1/county_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::CountyController < ApplicationController
   def index
     county = County.all
-    if county.empty? 
+    #require 'pry'; binding.pry 
+    if county.empty? == true
         render json: { error: 'API Rate Limit' }, status: 503
     else
         render json: CountySerializer.counties(county), status: 200
